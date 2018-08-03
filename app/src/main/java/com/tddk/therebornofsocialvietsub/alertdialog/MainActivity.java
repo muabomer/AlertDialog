940e2834c1d4934addf58150c76ec6abcc50f869
Admin <muabomer@gmail.com>
2018-08-03 20:37:12 +0700

    "a"

---
+++ app/src/main/java/com/tddk/therebornofsocialvietsub/alertdialog/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tddk.therebornofsocialvietsub.alertdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
